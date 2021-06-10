fba7d1a6302b81e4a9ad1d020470f5529aa8a2c5
robin bouma <72192756+robinbouma7@users.noreply.github.com>
2021-06-10 13:40:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,15 @@
 #include <pspctrl.h>
 #include "glib2d.h"
 
+float x = 12.5, y = 136;
+bool input = false; 
+
 
 PSP_MODULE_INFO("game-test", 0, 1, 1);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
 
 
-/stops the game.
+//stops the game.
 //this is just code that works, so dont change this. (upcoming 3 functions)
 int exit_callback(int arg1, int arg2, void *common) {
     sceKernelExitGame();
@@ -23,6 +26,31 @@ int callbackthread(SceSize args, void *argp) {
 
     sceKernelSleepThreadCB();
     return 0;
+}
+
+//wall variables
+int muurtop[8] = {47, 151, 0, 157, 0, 266, 6, 161};
+int muurleft[8] = {45, 0, 0, 0, 6, 6, 474, 474};
+int muurwidth[8] = {6, 43, 6, 6, 479, 479, 6, 6};
+int muurheight[8] = {68, 6, 116, 115, 6, 6, 105, 161};
+
+int drawwalls() {
+    //draw the walls    
+    for (int j = 0; j < 8; j++) {
+        g2dBeginRects(NULL); // No texture
+        g2dSetColor(WHITE);
+        g2dSetScaleWH(muurwidth[j],muurheight[j]);
+        g2dSetCoordXY(muurleft[j],muurtop[j]);
+        g2dAdd();
+        g2dEnd();
+    
+    }
+
+    return 0;
+}
+
+int collision() {
+    
 }
 
 
@@ -54,7 +82,7 @@ auto main() -> int {
                     //pspDebugScreenPrintf("up is pressed \n");
                     if (!input && y > 12.5){
                         y-=15;
-                        score-=5;
+                        
                         input = true;
                     }
                     else {
@@ -67,7 +95,7 @@ auto main() -> int {
                     //pspDebugScreenPrintf("down is pressed \n");
                     if (!input && y < 259.5){
                         y+=15;
-                        score-=5;
+                        
                         input = true;
                     }
                     else {
@@ -80,7 +108,7 @@ auto main() -> int {
                     //pspDebugScreenPrintf("right is pressed \n");
                     if (!input && x < 467.5) {
                         x+=15;
-                        score-=5;
+                       
                         input = true;
                      }
                      else {
@@ -93,7 +121,7 @@ auto main() -> int {
                     //pspDebugScreenPrintf("left is pressed \n");
                     if (!input && x > 12.5){
                         x-=15;
-                        score-=5;
+                        
                         input = true;
                     }
 
@@ -103,40 +131,7 @@ auto main() -> int {
                      
                     
                 }
-                else if (ctrldata.Buttons & PSP_CTRL_RTRIGGER && ctrldata.Buttons & PSP_CTRL_SELECT) {
-                    //muur collision aan en uit zetten
-                    if (!input && wallc) {
-                        wallc = false;
-                        input = true;
-                    }
-                    else if (!input && !wallc) {
-                        wallc = true;
-                        input = true;
-                    } 
-                    else {
-            
-                    }
-
-                }
-                else if (ctrldata.Buttons & PSP_CTRL_LTRIGGER && ctrldata.Buttons & PSP_CTRL_SELECT) {
-                    //finish collision aan en uit zetten
-                    if (!input && finishc) {
-                        finishc = false;
-                        input = true;
-                        
-                    }
-                    else if (!input && !finishc) {
-                        finishc = true;
-                        input = true;
-                        
-                    } 
-                    else {
-            
-                    }
-
-                }
-
-
+                
                 
                 else {
                     //als je niks indruk word hier input false. zodat je de knoppen moet klikken en niet in houden
